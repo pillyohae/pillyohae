d4e2f49f4359c8066397e6fc35bfa51eb3b77674
Weseunghyun <bible091192@gmail.com>
2025-01-16 20:48:27 +0900

    add: Valid UserCreateRequestDto

--- src/main/java/com/example/pillyohae/user/dto/UserCreateRequestDto.java
+++ src/main/java/com/example/pillyohae/user/dto/UserCreateRequestDto.java
@@ -17,12 +17,18 @@ public class UserCreateRequestDto {
     @NotBlank(message = "이메일을 입력해주세요")
     private final String email;
 
+    @NotBlank(message = "전화번호를 입력해주세요")
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "올바른 전화번호 형식이 아닙니다")
     private final String phoneNumber;
 
+    @NotBlank(message = "우편번호를 입력해주세요")
+    @Pattern(regexp = "^\\d{5}$", message = "올바른 우편번호 형식이 아닙니다")
     private final String postcode;
 
+    @NotBlank(message = "도로명 주소를 입력해주세요")
     private final String roadAddress;
 
+    @NotBlank(message = "상세 주소를 입력해주세요")
     private final String detailAddress;
 
     @Pattern(
