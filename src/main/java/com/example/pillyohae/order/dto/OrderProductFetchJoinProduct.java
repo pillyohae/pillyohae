67f82caada1893922fa4a3050bba7009dbc8f295
thdwj <thdwjdgkr123@naver.com>
2025-01-24 19:35:36 +0900

    refactor: orderProduct와 product join 추거

---
+++ src/main/java/com/example/pillyohae/order/dto/OrderProductFetchJoinProduct.java
@@ -0,0 +1,16 @@
+package com.example.pillyohae.order.dto;
+
+import com.example.pillyohae.order.entity.OrderProduct;
+import com.example.pillyohae.product.entity.Product;
+import com.querydsl.core.annotations.QueryProjection;
+
+
+public class OrderProductFetchJoinProduct {
+    OrderProduct orderProduct;
+    Product product;
+    @QueryProjection
+    public OrderProductFetchJoinProduct(OrderProduct orderProduct, Product product) {
+        this.orderProduct = orderProduct;
+        this.product = product;
+    }
+}
