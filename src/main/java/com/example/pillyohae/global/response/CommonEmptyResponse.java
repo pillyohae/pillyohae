65c847f8474dd0d453375ec287e94b98d2a7f242
thdwj <thdwjdgkr123@naver.com>
2025-02-05 22:34:21 +0900

    removed cached

--- src/main/java/com/example/pillyohae/global/response/CommonEmptyResponse.java
+++
@@ -1,7 +0,0 @@
-package com.example.pillyohae.global.response;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties // Ensures serialization for an empty object
-public record CommonEmptyResponse() {
-}
